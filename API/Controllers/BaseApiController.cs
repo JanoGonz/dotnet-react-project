645913be6e85e283bd6fbafb0146807949393382
Alejandro González <alejandro.gdp.1993@gmail.com>
2022-03-30 21:55:47 +0200

    Esqueleto parte 1

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //Ruta es igual al nombre del controlador - Controller
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
